7238ce47636f728587ad46e897b5ce21cd0883e0
kzadvorniy <kir594793@mail.ru>
2012-10-29 16:41:24 +0400

    GRSGBCC

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frog::Application.config.secret_token = 'e2f5130459ece77562f9b37e917cb90002a34571c1cd1e680da61bea5063402023310a1eb8e0c53abb31e5f7b4ebd0ea318e935bfa3230c78ec85fb7629d6c51'
